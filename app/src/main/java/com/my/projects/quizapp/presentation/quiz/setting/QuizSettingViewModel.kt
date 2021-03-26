c88aaceaf907e18149b2313b3af890b596fd876b
DevKazonovic <kazonovic.yasser.69@gmail.com>
2021-03-26 20:26:45 +0100

    Fix Style & Theme issues for dark theme

---
+++ app/src/main/java/com/my/projects/quizapp/presentation/quiz/setting/QuizSettingViewModel.kt
@@ -0,0 +1,11 @@
+package com.my.projects.quizapp.presentation.quiz.setting
+
+import androidx.lifecycle.ViewModel
+import javax.inject.Inject
+
+class QuizSettingViewModel @Inject constructor() : ViewModel() {
+
+
+
+
+}
